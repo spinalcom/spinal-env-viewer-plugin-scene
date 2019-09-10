7e6a4dfbda18e8bf65a50d6e7699b42fe16219be
LorisC <loris@ciprin.fr>
2019-09-10 15:26:35 +0200

    remove console.log

--- src/buttons/ButtonLoadModel.js
+++ src/buttons/ButtonLoadModel.js
@@ -25,7 +25,6 @@ export class ButtonLoadModel extends SpinalContextApp {
   }
   
   openPanel(option) {
-    console.log(window.spinal.SpinalForgeViewer)
     window.spinal.SpinalForgeViewer.loadModelFromBimFile(option.selectedNode)
   }
 }
